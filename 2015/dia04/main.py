735bc78f23381c76b2081418a88cc942c2bc9c09
rcsalomao <rafael.c.salomao@gmail.com>
2024-12-04 17:08:57 -0300

    feat: dias 01,02,03,04,05 - 2015

---
+++ 2015/dia04/main.py
@@ -0,0 +1,29 @@
+import hashlib
+
+input = open("input.txt", "r").read().strip()
+# print(input)
+
+
+def find_num(n_zeros, max_tries=100000000):
+    zeros = "".join(["0" for _ in range(n_zeros)])
+    for num in range(max_tries):
+        secret = input + str(num)
+        md5_hash = hashlib.md5(secret.encode())
+        if md5_hash.hexdigest()[0 : len(zeros)] == zeros:
+            break
+    return num
+
+
+def parte_1():
+    num = find_num(5)
+    print(num)
+
+
+def parte_2():
+    num = find_num(6)
+    print(num)
+
+
+if __name__ == "__main__":
+    parte_1()
+    parte_2()
---
+++ 2015/dia05/main.py
@@ -0,0 +1,35 @@
+import re
+
+input = open("input.txt", "r").read().strip()
+# print(input)
+
+
+def parte_1():
+    pattern_1 = r"[aeiou]"
+    pattern_2 = r"(\w)\1"
+    pattern_3 = r"ab|cd|pq|xy"
+    n_nice_strings = 0
+    for string in input.split():
+        match_1 = re.findall(pattern_1, string)
+        match_2 = re.findall(pattern_2, string)
+        match_3 = re.findall(pattern_3, string)
+        if len(match_1) > 2 and len(match_2) and not len(match_3):
+            n_nice_strings += 1
+    print(n_nice_strings)
+
+
+def parte_2():
+    pattern_1 = r"(\w\w)\w*\1"
+    pattern_2 = r"(\w)\w\1"
+    n_nice_strings = 0
+    for string in input.split():
+        match_1 = re.findall(pattern_1, string)
+        match_2 = re.findall(pattern_2, string)
+        if len(match_1) and len(match_2):
+            n_nice_strings += 1
+    print(n_nice_strings)
+
+
+if __name__ == "__main__":
+    parte_1()
+    parte_2()
